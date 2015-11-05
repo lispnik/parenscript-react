1763c1271568d0077760c0c836784c03ec1723bf
Matthew Kennedy <burnsidemk@gmail.com>
2015-11-05 09:05:55 -0600

    Mixin support

--- parenscript-react.lisp
+++ parenscript-react.lisp
@@ -17,22 +17,45 @@
 (defpsmacro is-valid-element (object)
   `(React.isValidElement ,object))
 
-(defpsmacro defcomponent (name methods &body body)
+(defpsmacro defcomponent (name mixins methods &body body)
   `(psetq ,name
           (React.createClass
            (create 'display-name ',name
                    ,@(mapcan #'(lambda (method)
-                                 (destructuring-bind (defun-ignore name args &body body) method
-                                   (declare (ignore defun-ignore))
+                                 (destructuring-bind (name args &body body) method
                                    (list name `(lambda ,args ,@body))))
                              methods)))))
 
 (ps
-  (defcomponent *Foo
-      ((defun render (foo bar baz)
-         (+ 1 2))
+ (progn
+   (psetq SetIntervalMixin
+          (create 'willComponentMount
+                  (lambda ()
+                    (setf (@ this intervals) '()))
 
-       (defun component-did-mount ()
-         (console.log "lol")))))
+                  'setInterval
+                  (lambda (&rest intervals)
+                    (setf (@ this intervals) (ps:mapcar 'set-interval intervals))) ;; FIXME
+
+                  'componentWillUnmount
+                  (lambda () (map clear-interval (@ this inntervals)))))
+   (defcomponent TockTock (SetIntervalMixin)
+     ((render ()
+              (create-element "p"
+                              (concatenate 'string "React has been doing this for "
+                                           (@ this state seconds) " seconds"))))
+     )))
+
+(ps* *ps-lisp-library*)
+(ps
+
+
+
+ (defcomponent *Foo ()
+   ((render (foo bar baz)
+            (+ 1 2))
+
+    (component-did-mount ()
+                         (console.log "lol")))))
 
 #.(setf (readtable-case *readtable*) :upcase)
